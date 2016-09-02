0342d516af127ea5b2cab157a0ee39433375517a
Andrew Bayer <andrew.bayer@gmail.com>
2016-09-02 11:10:50 -0400

    Update withTower.groovy

--- vars/withTower.groovy
+++ vars/withTower.groovy
@@ -1,9 +1,12 @@
 import org.apache.commons.lang.RandomStringUtils
 
 def call (host, credentials, Closure body){
+    echo "I'm in a call"
     dir ("tmp/${RandomStringUtils.random(9, true, true)}") {
+        echo "I'm in a dir"
         withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "$credentials",
                           usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+                              echo "I'm in creds"
             writeFile file: './.tower_cli.cfg',
                     text: "host: $host\nusername: $env.USERNAME \npassword: $env.PASSWORD"
             body()
